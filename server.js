688877e5c0abcc7058e8348d984d8ef5b270bf75
aniket sharma <aniket965.as@gmail.com>
2017-04-19 02:08:27 +0530

    added date check

--- server.js
+++ server.js
@@ -26,40 +26,80 @@ var data = {
     Date: "22 august"
 };
 
-     mongoose.connect('mongodb://aniket965:9818484049as@ds163340.mlab.com:63340/eventlists');
+mongoose.connect('mongodb://aniket965:9818484049as@ds163340.mlab.com:63340/eventlists');
 
 
 /* for adding data to database*/
+
+
+
 app.get('/adddataprojects:data', enc, function (req, res) {
+
+
     var pro = mongoose.model('pro', Schema);
 
-     var item = pro(finddata(req.params.data)).save(function (err) {
-         if (err) throw err;
-         
-         console.log(finddata(req.params.data))
+
+    /**
+     * saves in database
+     */
+ var isDatedAlready = false;
+    pro.find({}, function (err, datapro) {
+        if (err) throw err;
+        for (var i = 0; i < datapro.length; i++) {
+            if (datapro[i].Date === finddata(req.params.data).Date) {
+
+            isDatedAlready =true;
+            break;
+
+            }
+        }
+
+        if(isDatedAlready != true) {
+             var item = pro(finddata(req.params.data)).save(function (err) {
+                    if (err) throw err;
+                    console.log(finddata(req.params.data))
+                });
+        }
+
+    });
 
 
-     });
 
 });
+
+
+
 
 /*finds data from the parameters*/
 
 function finddata(data) {
-     
-   var itemdata =  data.split("*");
-   var id = itemdata[0];
-   var title = itemdata[1];
-   var eventLocation = itemdata[2];
-   var date = itemdata[3];
-   return {id : id,
-       title :title,
-       eventLocation : eventLocation,
-       Date :date
+
+    var itemdata = data.split("*");
+    var id = itemdata[0];
+    var title = itemdata[1];
+    var eventLocation = itemdata[2];
+    var date = itemdata[3];
+    return {
+        id: id,
+        title: title,
+        eventLocation: eventLocation,
+        Date: date
     }
-} 
+}
 
 
- 
+
+
+function filterdata(data) {
+
+
+
+}
+
+/**
+ * listens the port on server
+ */
+
+
 var port = process.env.PORT || 1337;
 app.listen(port);
